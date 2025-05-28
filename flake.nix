4d36a1e49de594f37bb4467af34afe843ea1b130
Michał Jerzy Wierzbicki <michal.jerzy.wierzbicki@amentum.studio>
2025-05-28 12:15:59 +0200

    add nix

---
+++ flake.nix
@@ -0,0 +1,95 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    nixpkgs-python = {
+      url = "github:cachix/nixpkgs-python";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    systems.url = "github:nix-systems/default";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-python,
+      devenv,
+      systems,
+      ...
+    }@inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+      projects = [
+        "default"
+        "greeter"
+      ];
+      forEachProject = nixpkgs.lib.genAttrs projects;
+    in
+    {
+      packages = forEachSystem (
+        system:
+        forEachProject (
+          project:
+          let
+            config = self.devShells.${system}.${project}.config;
+          in
+          {
+            "${project}-devenv-up" = config.procfileScript;
+            "${project}-devenv-test" = config.test;
+          }
+        )
+      );
+
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs-python = nixpkgs-python.packages.${system};
+          python-with-lsp =
+            version:
+            (pkgs-python."${version}".withPackages (
+              ps: with ps; [
+                python-lsp-server
+                python-lsp-jsonrpc
+                python-lsp-ruff
+              ]
+            ));
+          greeter = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                enterShell = ''
+                  echo "devenv==greeter";
+                '';
+                languages.python = {
+                  enable = true;
+                  directory = "./containers/greeter/buildcontext";
+                  package = python-with-lsp "3.13.2";
+                  uv = {
+                    enable = true;
+                    sync = {
+                      # pyproject.toml has to exist
+                      enable = true;
+                    };
+                  };
+                };
+              }
+            ];
+          };
+        in
+        {
+          default = greeter;
+          greeter = greeter;
+        }
+      );
+    };
+}
